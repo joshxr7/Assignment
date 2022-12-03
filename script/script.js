782876a50b869feea9f33b6c16e757afacdad9b9
josh rogerson <jrogerson@my.bcit.ca>
2022-12-02 21:24:57 -0800

    created dark mode so that background of website, sidebar, texbox, and buttons get darker, also changed many elements of html and css to adjust for it, with new div classes to help make them work

--- script/script.js
+++ script/script.js
@@ -1,17 +1,32 @@
-const darkModebutton = document.querySelector(".dark_theme");
+const darkModebutton = document.querySelector("#dark_theme");
 
 function changeDarkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  let sidebar_dark = document.getElementById("sideBar");
+  sidebar_dark.classList.toggle("dark-mode-textbox");
+
+  let background_dark = document.body;
+  background_dark.classList.toggle("dark-mode");
+
+  let textbox_dark = document.getElementById("TextBox");
+  textbox_dark.classList.toggle("dark-mode-textbox");
+  
+  let darkNewNotebutton = document.getElementById("New_note");
+  darkNewNotebutton.classList.toggle("dark-mode-green");
+
+  let darkSavebutton = document.getElementById("save_button");
+  darkSavebutton.classList.toggle("dark-mode-green");
+
+  let darkCancelbutton = document.getElementById("cancel");
+  darkCancelbutton.classList.toggle("dark-mode-red");
 }
 
 darkModebutton.addEventListener("click", changeDarkMode);
 
 function changeTextDarkMode() {
-  const initialtext = "Dark Theme";
+  const initialtext = "Light Theme";
 
-  if (darkModebutton.textContent === "Dark Theme") {
-    darkModebutton.textContent = "Light Theme";
+  if (darkModebutton.textContent === "Light Theme") {
+    darkModebutton.textContent = "Dark Theme";
   } else {
     darkModebutton.textContent = initialtext;
   }
@@ -19,7 +34,7 @@ function changeTextDarkMode() {
 
 darkModebutton.addEventListener("click", changeTextDarkMode);
 
-const cancelPagebutton = document.querySelector(".cancel");
+const cancelPagebutton = document.querySelector("#cancel");
 
 function cancelChange() {
   document.getElementById("cancelButton").style.visibility = "hidden";
@@ -27,39 +42,18 @@ function cancelChange() {
 
 cancelPagebutton.addEventListener("click", cancelChange);
 
-const newPagebutton = document.querySelector(".New_note");
+const newPagebutton = document.querySelector("#New_note");
 
 function reverseCancelChange() {
   document.getElementById("cancelButton").style.visibility = "visible";
 
-  
-    document.getElementById("TextBox").value = "";
-  
-    
+  document.getElementById("TextBox").value = "";
 }
 
 newPagebutton.addEventListener("click", reverseCancelChange);
 
-let notesArray = {
-  title: "note one",
-  body: "this is my first note",
-};
+let notesArray = [
+  { title: "note one", body: "this is my first note" },
+  { title: "note two", body: "this is my second note" },
+];
 
-const saveButton = document.querySelector(".save_button");
-
-function textboxChange() {
-  user_var = ""
-
-  alert("Print the title of the Note you want to save");
-
-
- 
- 
-  document.body.appendChild(user_var);
-
-//   Add a new JS object to the notesArray with the provided title and the text in the text area as the body
-// Add a list item to the sidebar with the title of the JS object you just added to the array
-
-}
-
-saveButton.addEventListener("click", textboxChange)
